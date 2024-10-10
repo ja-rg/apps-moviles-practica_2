a69cde4c07e3f7bdfe717057e1abf0c04f48338e
Alejandro Rosales <j.alex.rosales@proton.me>
2024-10-10 15:49:25 -0600

    App index directory con solamente pestañas

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
-import { Stack } from "expo-router";
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+
 
 export default function RootLayout() {
+  useFonts({
+    'IndivisaTextSans': require('@/assets/fonts/IndivisaTextSans-Regular.ttf'),
+  });
+
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
     </Stack>
   );
 }
